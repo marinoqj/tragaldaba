1d0f8105a4db0c9b0a59fa69e5331202c67bbfcf
marino <marino.quijada@golemdr.es>
2021-09-14 11:51:14 +0200

    Cambios para que funcione en local (fallan los profiles)

--- src/main/java/es/golemdr/tragaldaba/repository/UsuariosRepository.java
+++ src/main/java/es/golemdr/tragaldaba/repository/UsuariosRepository.java
@@ -3,6 +3,7 @@ package es.golemdr.tragaldaba.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.golemdr.tragaldaba.domain.Usuario;
 import es.golemdr.tragaldaba.repository.custom.UsuariosRepositoryCustom;
@@ -10,7 +11,7 @@ import es.golemdr.tragaldaba.repository.custom.UsuariosRepositoryCustom;
 
 
 
-
+@Repository
 public interface UsuariosRepository extends JpaRepository<Usuario, Long>, UsuariosRepositoryCustom{
 
 }
--- src/main/java/es/golemdr/tragaldaba/repository/custom/UsuariosRepositoryCustom.java
+++ src/main/java/es/golemdr/tragaldaba/repository/custom/UsuariosRepositoryCustom.java
@@ -7,7 +7,7 @@ import es.golemdr.tragaldaba.domain.Usuario;
 
 
 
-@Repository
+
 public interface UsuariosRepositoryCustom {
 	
 	public Usuario recuperarUsuarioLogin(String login);
